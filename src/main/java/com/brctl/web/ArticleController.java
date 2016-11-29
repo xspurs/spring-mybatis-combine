b2e0cfc63d757b08c75819547c9de3c1d9e9d1b2
Duan Xiaoxing <duanxiaoxing@pdmi.cn>
2016-11-29 13:06:51 +0800

    integrate with log4j and slf4j

--- src/main/java/com/brctl/web/ArticleController.java
+++ src/main/java/com/brctl/web/ArticleController.java
@@ -2,6 +2,8 @@ package com.brctl.web;
 
 import com.brctl.domain.Article;
 import com.brctl.service.IArticleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -20,20 +22,23 @@ import java.util.List;
 @RequestMapping("/article")
 public class ArticleController {
 
+    private final static Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
     @Autowired
     private IArticleService articleService;
 
-
     @RequestMapping(value = "/{id}")
     @ResponseBody
-    public HttpEntity<Article> requestBlogById(@PathVariable String id) {
+    public HttpEntity<Article> requestArticleById(@PathVariable String id) {
+        logger.info("==========根据ID:{}查询文章==========", id);
         Article article = articleService.findById(id);
         return new ResponseEntity<>(article, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/all")
     @ResponseBody
-    public HttpEntity<List<Article>> requestBlogById() {
+    public HttpEntity<List<Article>> requestAllArticles() {
+        logger.info("==========查询全部文章==========");
         List<Article> articles = articleService.findAll();
         return new ResponseEntity<>(articles, HttpStatus.OK);
     }
